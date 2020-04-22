c438dc6c3daf2ba06dc53ba0233e37ad4d46bfa4
anwanfei <546513254@qq.com>
2020-04-22 10:55:08 +0800

    retrofit get2  get3

--- retrofit/src/main/java/com/anfly/retrofit/ApiServer.java
+++ retrofit/src/main/java/com/anfly/retrofit/ApiServer.java
@@ -1,8 +1,12 @@
 package com.anfly.retrofit;
 
+import java.util.Map;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface ApiServer {
     //必须以 “/” 结尾
@@ -10,4 +14,11 @@ public interface ApiServer {
 
     @GET("dish_list.php?stage_id=1&limit=20&page=1")
     Call<ResponseBody> get1();
+
+    @GET("dish_list.php")
+    Call<ResponseBody> get2(@Query("stage_id") String stage_id, @Query("limit") String limit, @Query("page") String page);
+
+    @GET("dish_list.php")
+    Call<ResponseBody> get3(@QueryMap Map<String, String> map);
+
 }
--- retrofit/src/main/java/com/anfly/retrofit/MainActivity.java
+++ retrofit/src/main/java/com/anfly/retrofit/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -21,6 +22,8 @@ import retrofit2.Retrofit;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn_get1;
+    private Button btn_get2;
+    private Button btn_get3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_get1 = (Button) findViewById(R.id.btn_get1);
 
         btn_get1.setOnClickListener(this);
+        btn_get2 = (Button) findViewById(R.id.btn_get2);
+        btn_get2.setOnClickListener(this);
+        btn_get3 = (Button) findViewById(R.id.btn_get3);
+        btn_get3.setOnClickListener(this);
     }
 
     @Override
@@ -41,7 +48,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_get1:
                 get1();
                 break;
+            case R.id.btn_get2:
+                get2();
+                break;
+            case R.id.btn_get3:
+                get3();
+                break;
         }
+    }
+
+    private void get3() {
+        //retrofit对象
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(ApiServer.baseUrl).build();
+
+        //获取服务对象
+        ApiServer apiServer = retrofit.create(ApiServer.class);
+
+        //服务对象调用方法获得call对象
+        HashMap<String, String> map = new HashMap<>();
+        map.put("stage_id", "1");
+        map.put("limit", "20");
+        map.put("page", "1");
+        Call<ResponseBody> call = apiServer.get3(map);
+
+        //call执行请求
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            try {
+                String json = response.body().string();
+                FoodBean foodBean = new Gson().fromJson(json, FoodBean.class);
+                btn_get3.setText(foodBean.getData().get(0).getTitle());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onFailure(Call<ResponseBody> call, Throwable t) {
+            Log.e("TAG", "网络错误：" + t.getMessage());
+        }
+    });
+}
+
+    private void get2() {
+        //retrofit对象
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(ApiServer.baseUrl).build();
+
+        //获取服务对象
+        ApiServer apiServer = retrofit.create(ApiServer.class);
+
+        //服务对象调用方法获得call对象
+        Call<ResponseBody> call = apiServer.get2("1", "20", "1");
+
+        //call执行请求
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    String json = response.body().string();
+                    FoodBean foodBean = new Gson().fromJson(json, FoodBean.class);
+                    btn_get2.setText(foodBean.getData().get(0).getTitle());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("TAG", "网络错误：" + t.getMessage());
+            }
+        });
+
     }
 
     private void get1() {
